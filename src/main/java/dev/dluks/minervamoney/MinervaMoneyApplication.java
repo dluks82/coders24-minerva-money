45b6d2f0290234691250d85f1245a445fe3d9dde
Diogo Oliveira <dluks82@gmail.com>
2024-11-05 13:23:44 -0300

    :wrench: initial commit

---
+++ src/main/java/dev/dluks/minervamoney/MinervaMoneyApplication.java
@@ -0,0 +1,13 @@
+package dev.dluks.minervamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervaMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinervaMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dluks.minervamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
