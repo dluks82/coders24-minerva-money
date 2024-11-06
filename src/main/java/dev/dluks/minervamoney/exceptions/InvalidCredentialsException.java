3dc1e6e1b184ba486723fff47ae6856ee3540e55
Diogo Oliveira <dluks82@gmail.com>
2024-11-06 13:40:44 -0300

    :sparkles: Add `InvalidCredentialsException`

---
+++ src/main/java/dev/dluks/minervamoney/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package dev.dluks.minervamoney.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
