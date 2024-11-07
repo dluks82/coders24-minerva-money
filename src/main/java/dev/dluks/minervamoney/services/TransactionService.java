581f876e00cfe24da7b30efe8c3d542f6d7c92c1
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-07 19:23:38 -0300

    change: change in the methods to perform the search via accoutId

--- src/main/java/dev/dluks/minervamoney/services/TransactionService.java
+++ src/main/java/dev/dluks/minervamoney/services/TransactionService.java
@@ -2,12 +2,17 @@ package dev.dluks.minervamoney.services;
 
 import dev.dluks.minervamoney.dtos.transaction.TransactionDTO;
 import dev.dluks.minervamoney.dtos.transaction.TransactionRequestDTO;
+import dev.dluks.minervamoney.entities.Account;
+import dev.dluks.minervamoney.entities.CustomUserDetails;
 import dev.dluks.minervamoney.entities.Transaction;
+import dev.dluks.minervamoney.repositories.AccountRepository;
 import dev.dluks.minervamoney.repositories.TransactionRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -24,29 +30,37 @@ import java.util.stream.Collectors;
 public class TransactionService {
 
     private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
     private final ModelMapper modelMapper = new ModelMapper();
 
     @Transactional(readOnly = true)
-    public List<TransactionDTO> getAllTransactions(){
-        return transactionRepository.findByDeletedFalse()
+    public List<TransactionDTO> getAllTransactions(UUID accountId) {
+        contaPerteceAoUsuario(accountId);
+
+        return transactionRepository.findByAccountIdAndDeletedFalse(accountId)
                 .stream()
                 .map(transaction -> modelMapper.map(transaction, TransactionDTO.class))
                 .collect(Collectors.toList());
     }
 
+
     @Transactional(readOnly = true)
-    public TransactionDTO getTransactionById(UUID id){
-        Transaction transaction = transactionRepository.findById(id)
+    public TransactionDTO getTransactionById(UUID accountId, UUID transactionId ) {
+        contaPerteceAoUsuario(accountId);
+
+        Transaction transaction = transactionRepository.findById(transactionId)
                 .orElseThrow(() -> new EntityNotFoundException("Transação não existe"));
         return modelMapper.map(transaction, TransactionDTO.class);
     }
 
     @Transactional
     public TransactionDTO createTransaction(
-            @RequestBody TransactionRequestDTO   request){
+
+            @RequestBody TransactionRequestDTO   request, UUID accountId) {
+        var account = contaPerteceAoUsuario(accountId);
         Transaction transaction = modelMapper.map(request, Transaction.class);
         transaction.setCategoryId(1L);
-        transaction.setAccountId(UUID.fromString("75386242-ab21-482b-bda4-52e2d7fcffb9"));
+        transaction.setAccount(account);
 
 
         transaction = transactionRepository.save(transaction);
@@ -54,7 +68,8 @@ public class TransactionService {
     }
 
     @Transactional
-    public void softDeleteTransaction(UUID id, String reason) {
+    public void softDeleteTransaction(UUID id, String reason, UUID accountId) {
+        contaPerteceAoUsuario(accountId);
         Transaction transaction = transactionRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Transaction not found"));
 
@@ -62,6 +77,17 @@ public class TransactionService {
         transactionRepository.save(transaction);
     }
 
+    private Account contaPerteceAoUsuario(UUID accountId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails currentUser = (CustomUserDetails) authentication.getPrincipal();
+
+        Optional<Account> account = accountRepository.findById(accountId);
+
+        if(account.isPresent() && !currentUser.getId().toString().equals(account.get().getUser().getId().toString())) {
+            throw new EntityNotFoundException("A conta não pertense ao usuario!");
+        }
+        return account.orElse(null);
+    }
 
 
 }
