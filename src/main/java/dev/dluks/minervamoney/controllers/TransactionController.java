1d9658ca4b3f3e01823bbc89b75321de7f1f750b
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-07 19:21:28 -0300

    change: endpoint changes

--- src/main/java/dev/dluks/minervamoney/controllers/TransactionController.java
+++ src/main/java/dev/dluks/minervamoney/controllers/TransactionController.java
@@ -1,10 +1,12 @@
 package dev.dluks.minervamoney.controllers;
 
 import dev.dluks.minervamoney.dtos.transaction.TransactionDTO;
+import dev.dluks.minervamoney.dtos.transaction.TransactionDeleteRequestDTO;
 import dev.dluks.minervamoney.dtos.transaction.TransactionRequestDTO;
 import dev.dluks.minervamoney.exceptions.CustomExceptionHandler;
 import dev.dluks.minervamoney.services.TransactionService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,39 +17,43 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/api/transactions")
+@RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class TransactionController {
 
     private final TransactionService transactionService;
 
-    @GetMapping()
-    public ResponseEntity<List<TransactionDTO>> getAllTransactions(){
-        return ResponseEntity.ok(transactionService.getAllTransactions());
+    @GetMapping("/{accountId}/transactions")
+    public ResponseEntity<List<TransactionDTO>> getAllTransactions(@PathVariable UUID accountId) {
+        return ResponseEntity.ok(transactionService.getAllTransactions(accountId));
     }
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<TransactionDTO> getTransactionById(@PathVariable UUID id) {
-        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    @GetMapping("/{accountId}/transactions/{transactionId}")
+    public ResponseEntity<TransactionDTO> getTransactionById(
+            @PathVariable UUID transactionId, @PathVariable UUID accountId) {
+        return ResponseEntity.ok(transactionService.getTransactionById(accountId, transactionId));
     }
 
-    @PostMapping
+    @PostMapping("/{accountId}/transactions")
     public ResponseEntity<TransactionDTO> createTransaction(
+            @PathVariable UUID accountId,
             @Valid @RequestBody TransactionRequestDTO request) {
             return new ResponseEntity<>(
-                transactionService.createTransaction(request),
+                transactionService.createTransaction(request, accountId),
                 HttpStatus.CREATED
         );
     }
 
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{accountId}/transactions/{transactionId}")
     public ResponseEntity<Void> deleteTransaction(
-            @PathVariable UUID id,
-            @RequestParam String reason) {
-        transactionService.softDeleteTransaction(id, reason);
+            @PathVariable UUID transactionId,
+            @PathVariable UUID accountId,
+            @RequestBody TransactionDeleteRequestDTO requestDTO) {
+        transactionService.softDeleteTransaction(transactionId, requestDTO.getReason(), accountId);
         return ResponseEntity.noContent().build();
     }
+
 
 }
