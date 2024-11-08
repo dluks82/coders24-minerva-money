3a82f44d003e42d235a3b72790be37e105ad17aa
Samuel Quaresma <squoliver@gmail.com>
2024-11-08 19:10:43 -0300

    Associação entre User e Category, criação de CategoryDTO, migration pra alterar a tabela categories

---
+++ src/main/java/dev/dluks/minervamoney/dtos/category/CategoryDTO.java
@@ -0,0 +1,12 @@
+package dev.dluks.minervamoney.dtos.category;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryDTO {
+
+    private String name;
+    private String description;
+}
--- src/main/java/dev/dluks/minervamoney/entities/Category.java
+++ src/main/java/dev/dluks/minervamoney/entities/Category.java
@@ -1,9 +1,12 @@
 package dev.dluks.minervamoney.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,10 +15,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -24,13 +28,32 @@ import java.util.UUID;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String description;
     private Boolean active;
     private LocalDateTime createdAt;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private User owner; // "null" se for uma "categoria base"
 
+    public boolean isBaseCategory() {
+        return owner == null;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name) &&
+                Objects.equals(owner, category.owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, owner);
+    }
 }
