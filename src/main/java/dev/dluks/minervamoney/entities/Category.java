78351923b94b0d8f129b02bd3e6b6e93a8cd5a73
Samuel Quaresma <squoliver@gmail.com>
2024-11-08 18:04:19 -0300

    Criação da classe Category

---
+++ src/main/java/dev/dluks/minervamoney/entities/Category.java
@@ -0,0 +1,36 @@
+package dev.dluks.minervamoney.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String description;
+    private Boolean active;
+    private LocalDateTime createdAt;
+
+
+
+}
