05fee1bbf66021a057ebd128178e44552527a4ae
Samuel Quaresma <squoliver@gmail.com>
2024-11-09 23:51:00 -0300

    Algumas rotas (criar categorias de base) e listar essas categorias, além de ajustes

--- src/main/java/dev/dluks/minervamoney/controllers/UserController.java
+++ src/main/java/dev/dluks/minervamoney/controllers/UserController.java
@@ -1,6 +1,9 @@
 package dev.dluks.minervamoney.controllers;
 
+import dev.dluks.minervamoney.dtos.category.CategoryDTO;
 import dev.dluks.minervamoney.dtos.user.UserProfileDTO;
+import dev.dluks.minervamoney.entities.Category;
+import dev.dluks.minervamoney.mappers.CategoryMapper;
 import dev.dluks.minervamoney.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,19 +12,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final CategoryMapper categoryMapper;
 
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<UserProfileDTO> authenticatedUser() {
-
         return ResponseEntity.ok(userService.authenticatedUserProfile());
+    }
 
+    @GetMapping("/categories/custom")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Set<CategoryDTO>> getUserCategories() {
+        Set<Category> userCategories = userService.getUserCategories();
+        return ResponseEntity.ok(userCategories.stream().map(categoryMapper::toDto).collect(Collectors.toSet()));
     }
 
 }
--- src/main/java/dev/dluks/minervamoney/services/CategoryService.java
+++ src/main/java/dev/dluks/minervamoney/services/CategoryService.java
@@ -1,11 +1,15 @@
 package dev.dluks.minervamoney.services;
 
+import dev.dluks.minervamoney.dtos.category.CategoryDTO;
 import dev.dluks.minervamoney.entities.Category;
 import dev.dluks.minervamoney.entities.User;
+import dev.dluks.minervamoney.mappers.CategoryMapper;
 import dev.dluks.minervamoney.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -14,12 +18,21 @@ import java.util.Set;
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
+    @Transactional(readOnly = true)
     public List<Category> getBaseCategories() {
         return categoryRepository.findByOwnerIsNull();
     }
 
     public Set<Category> getCustomCategoriesByUser(User user) {
         return categoryRepository.findByOwner(user);
+    }
+
+    @Transactional
+    public CategoryDTO createBaseCategory(Category category) {
+        category.setActive(true);
+        category.setCreatedAt(LocalDateTime.now());
+        return categoryMapper.toDto(categoryRepository.save(category));
     }
 }
