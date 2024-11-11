ab165591697fec810e7d16e6a9b58a7b8b4205c4
Samuel Quaresma <squoliver@gmail.com>
2024-11-10 22:16:02 -0300

    Endpoint para excluir uma custom categoria do usuário e tratamento de exceção caso essa categoria não exista

--- src/main/java/dev/dluks/minervamoney/repositories/CategoryRepository.java
+++ src/main/java/dev/dluks/minervamoney/repositories/CategoryRepository.java
@@ -3,13 +3,20 @@ package dev.dluks.minervamoney.repositories;
 import dev.dluks.minervamoney.entities.Category;
 import dev.dluks.minervamoney.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findByOwnerIsNull();
 
     Set<Category> findByOwner(User user);
+
+    @Query(value = "SELECT * FROM categories WHERE owner_id = :ownerId AND LOWER(name) = LOWER(:name)", nativeQuery = true)
+    Optional<Category> findByOwnerIdAndName(@Param("ownerId") UUID ownerId, @Param("name") String name);
 }
