3f8656b3eb8abc09394559eb1f31545a380fc4c4
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-10 22:28:26 -0300

    changed: included @manyToOne for categories.

--- src/main/java/dev/dluks/minervamoney/entities/Transaction.java
+++ src/main/java/dev/dluks/minervamoney/entities/Transaction.java
@@ -9,10 +9,8 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -57,8 +55,9 @@ public class Transaction {
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
-    @Column(name="category_id", nullable = false)
-    private Long categoryId;
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = false)
+    private Category category;
 
 
 
