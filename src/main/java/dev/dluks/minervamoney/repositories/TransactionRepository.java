235300939518c4733861a64064d7d80f14ff0e1e
Romulo Domingos <romulo.domingos11@gmail.com>
2024-11-11 00:24:48 -0300

    changed: included search by month and year and pagination

--- src/main/java/dev/dluks/minervamoney/repositories/TransactionRepository.java
+++ src/main/java/dev/dluks/minervamoney/repositories/TransactionRepository.java
@@ -1,7 +1,11 @@
 package dev.dluks.minervamoney.repositories;
 
 import dev.dluks.minervamoney.entities.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -22,4 +26,21 @@ public interface TransactionRepository extends JpaRepository<Transaction, UUID> 
             LocalDate startDate,
             LocalDate endDate
     );
+
+    Page<Transaction> findByAccountIdAndDeletedFalse(UUID accountId, Pageable pageable);
+
+    @Query("SELECT t FROM Transaction t WHERE " +
+            "t.account.id = :accountId AND " +
+            "t.deleted = false AND " +
+            "t.date BETWEEN :startDate AND :endDate")
+    Page<Transaction> findByAccountIdAndPeriod(
+            @Param("accountId") UUID accountId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            Pageable pageable
+    );
+
+
+
+
 }
