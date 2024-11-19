57665b41136b80dbd2b13deaa3885cc2d848a00e
Diogo Oliveira <dluks82@gmail.com>
2024-11-19 14:30:11 -0300

    :wrench: Add `BaseIntegrationTest` class and `TestContainer` dependency

--- src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
+++ src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
@@ -1,10 +1,11 @@
 package dev.dluks.minervamoney;
 
+import dev.dluks.minervamoney.config.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinervaMoneyApplicationTests {
+class MinervaMoneyApplicationTests extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
