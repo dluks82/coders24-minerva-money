7c9368b0b39514be5e84c21ed46456aa947b2eef
Diogo Oliveira <dluks82@gmail.com>
2024-11-19 18:22:17 -0300

    :wrench: Remove `minimum coverage` for now

--- src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
+++ src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MinervaMoneyApplicationTests extends BaseIntegrationTest {
+class MinervaMoneyApplicationTest extends BaseIntegrationTest {
 
     @Test
     void contextLoads() {
