6657e7a94730fe1ad039e512731c64cc81918e56
Diogo Oliveira <dluks82@gmail.com>
2024-11-21 12:28:42 -0300

    :wrench: Fix MinervaMoneyApplicationTests

--- src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
+++ src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class MinervaMoneyApplicationTests {
 
     @Test
-    void contextLoads() {
-        assertEquals(1, 1);
+    void shouldRemoveTestWhenDevelopingRealTests() {
+        assertTrue(true, "Delete this test when implementing real unit tests");
     }
-
 }
