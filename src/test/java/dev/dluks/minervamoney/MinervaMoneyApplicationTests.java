db0196607ccf121e20b8f798e8410e5f64764add
Diogo Oliveira <dluks82@gmail.com>
2024-11-21 12:19:16 -0300

    :wrench: Just remove `useless` imports and add one assertion on ApplicationTests

--- src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
+++ src/test/java/dev/dluks/minervamoney/MinervaMoneyApplicationTests.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 class MinervaMoneyApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1, 1);
     }
 
 }
